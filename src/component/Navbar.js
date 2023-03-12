959d1ce5e424c7887033ec26697d9b67c73ba211
annasrahmanto <annasrahmanto@gmail.com>
2023-03-12 22:20:58 +0700

    may finish

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,40 +1,37 @@
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
 
 function OffcanvasNavbar() {
+  // console.log(window.location)
   return (
     <>
-     
-        <Navbar  expand="sm" className="mb-3">
-          <Container fluid className="justify-content-end">
-
-            <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm"/>
-            <Navbar.Offcanvas
-              id="offcanvasNavbar-expand-sm"
-              aria-labelledby="offcanvasNavbarLabel-expand-sm"
-              placement="end"
-            >
-              <Offcanvas.Header closeButton className="pe-5">
-                
-                <Offcanvas.Title id="offcanvasNavbarLabel-expand-sm">
-                  BCR
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-center flex-grow-1 pe-3 gap-3">
-                  <Nav.Link href="#action1">Our Service</Nav.Link>
-                  <Nav.Link href="#action2">Why Us</Nav.Link>
-                  <Nav.Link href="#action2">Testimonial</Nav.Link>
-                  <Nav.Link href="#action2">FAQ</Nav.Link>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      
+      <Navbar expand="sm" className="mb-3">
+        <Container fluid className="justify-content-end">
+          <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-sm"
+            aria-labelledby="offcanvasNavbarLabel-expand-sm"
+            placement="end"
+            className="w-50"
+          >
+            <Offcanvas.Header closeButton className="pe-5">
+              <Offcanvas.Title id="offcanvasNavbarLabel-expand-sm">
+                BCR
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-center flex-grow-1 pe-3 gap-3">
+                <Nav.Link href="/OurServices">Our Service</Nav.Link>
+                <Nav.Link href="WhyUs">Why Us</Nav.Link>
+                <Nav.Link href="/Testimonial">Testimonial</Nav.Link>
+                <Nav.Link href="/FAQ">FAQ</Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </>
   );
 }
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Header from "./header";
 import Footer from "./footer";
+import PublicRoutes from "../config/router";
+
+
 
 const Layout = (props) => {
   return (
     <>
-      <Header/>
-      <Footer/>
+      <Header />
+      <PublicRoutes />
+      <Footer />
+      
     </>
   );
 };
